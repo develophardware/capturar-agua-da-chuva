e9605ae6ef7ba56000a20b21f535c4c445663968
Paulo Rodrigues <paulinhochok@hotmail.com>
2021-05-28 11:31:11 -0300

    Componente Servor Motor

--- front/js/motor_calha.js
+++ front/js/motor_calha.js
@@ -1,12 +1,26 @@
-var five = require("johnny-five");
-var board = new five.Board();
+const { Board, Servo } = require("johnny-five");
+const readline = require("readline");
 
-board.on("ready", function() {
-
-    var motor_calha = new five.Servo({
-        pin: 7,
-        center: true
-    });
-
-    motor_calha.to(180);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+
+const board = new Board();
+
+board.on("ready", () => {
+    const servo = new Servo(2);
+
+    //servo.to(0, 60); 
+
+    rl.setPrompt("SERVO TEST (0-180)> ");
+    rl.prompt();
+
+    rl.on("line", (line) => {
+        servo.to(+line.trim());
+        rl.prompt();
+    }).on("close", () => process.exit(0));
+});
+
+//Referencia de posição do servo, lado direito enchendo com calaha lado esquedo levantada 80º pra menos <-
+// Referencia de posição servo, lado esquerdo enchendo com calha lado direito levantada posição 93° pra mais ->
