9721b32add7ab46323c0a7811dec7f7190ccf173
aldsandrade <andrelds.andrade@gmail.com>
2021-10-06 19:16:56 -0300

    modificado script para trabalhar com as funcoes dos sensores

--- src/app.js
+++ src/app.js
@@ -8,18 +8,18 @@ const formatacoes = require('./utility/formatacoes');
 console.log('** APLICAÇÃO INICIALIZADA **');
 
 async function start() {
-    // Ao inicializar a aplicação, o valor da variavel de ambiente VOLUME_ECONOMIZADO é setada como a soma de ambas as cisternas
-    let retorno = await sensores.processaSensores();
-    process.env.VOLUME_ECONOMIZADO = retorno.cisterna1 + retorno.cisterna2;
-
-    // Ao inicializar a aplicação, o valor da variavel de ambiente DATA_ECONOMIA é setada como a data do dia
-    let data_atual = formatacoes.convertUTCDateToLocalDate(new Date());
-    process.env.DATA_ECONOMIA = data_atual
+    // Inicializa a leitura dos sensores
+    sensores.processaSensores();
 
     // Gera um loop de execução da função que faz todo o processo de atualização de volumes e economia
     setInterval(async function() {
-        await controller.verificaEconomiaRealizada();
-      }, 1000 * 60 * 1); // 1000ms = 1 segundo, * 60 = 1 minuto, * 5 = 5 minutos
+      process.env.VOLUME_ECONOMIZADO = process.env.VOLUME_CISTERNA1 + process.env.VOLUME_CISTERNA2
+      // Ao inicializar a aplicação, o valor da variavel de ambiente DATA_ECONOMIA é setada como a data do dia
+      let data_atual = formatacoes.convertUTCDateToLocalDate(new Date());
+      process.env.DATA_ECONOMIA = data_atual;
+
+      await controller.verificaEconomiaRealizada();
+    }, 1000 * 60 * 1); // 1000ms = 1 segundo, * 60 = 1 minuto, * 5 = 5 minutos
     
 }
 
