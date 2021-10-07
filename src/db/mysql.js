49e7aef7ce4c957cbbe5a49099126d98fcaa03f7
Paulo Rodrigues <paulinhochok@hotmail.com>
2021-10-07 19:05:40 -0300

    Corrigindo Script aplicação funcionando

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,12 +1,12 @@
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname,"../.env") })
+require("dotenv").config({ path: path.join(__dirname, "../.env") })
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 module.exports = {
@@ -14,7 +14,7 @@ module.exports = {
      * Função que verifica o volume de todos os recipientes
      * @returns sem retorno
      */
-	queryVolumes: function() {
+    queryVolumes: function() {
         return new Promise((resolve, reject) => {
             pool.getConnection(function(error, connection) {
                 if (error) {
@@ -22,9 +22,9 @@ module.exports = {
                     callback(true);
                     reject();
                 }
-    
+
                 let sql = 'SELECT * FROM monitoramento';
-    
+
                 connection.query(sql, [], function(error, results) {
                     if (error) {
                         console.error(error);
@@ -33,17 +33,17 @@ module.exports = {
                     }
                     resolve(results);
                 });
-                
+
             });
         });
-	},
-	/**
+    },
+    /**
      * Função que atualiza o volume dos recipientes
      * @param {number} recipiente_id id do recipiente (5 ou 15)
      * @param {number} volume volume atual do recipiente
      * @returns sem retorno
      */
-	atualizaVolume: function(recipiente_id, volume) {
+    atualizaVolume: function(recipiente_id, volume) {
         return new Promise((resolve, reject) => {
             pool.getConnection(function(error, connection) {
                 if (error) {
@@ -51,10 +51,10 @@ module.exports = {
                     callback(true);
                     reject();
                 }
-    
+
                 let sql = 'UPDATE monitoramento SET volume = ? WHERE recipiente_id = ?';
-    
-                connection.query(sql, [ volume, recipiente_id ], function(error) {
+
+                connection.query(sql, [volume, recipiente_id], function(error) {
                     if (error) {
                         console.error(error);
                         callback(true);
@@ -65,7 +65,7 @@ module.exports = {
                 });
             });
         })
-	},
+    },
     /**
      * Função que cria o registro de volume economizado no dia
      * @param {number} volume volume obtido no dia
@@ -80,22 +80,22 @@ module.exports = {
                     callback(true);
                     reject();
                 }
-    
+
                 let sql = 'INSERT INTO economia_realizada (data, qtd_litros_economizados) VALUES (?, ?)';
-    
-                connection.query(sql, [ data, qtd_litros_economizados ], function(error) {
+
+                connection.query(sql, [data, qtd_litros_economizados], function(error) {
                     if (error) {
                         console.error(error);
                         callback(true);
                         reject();
                     }
-                    
+
                     console.log('Criado registor de economia no dia ' + data + '. Volume registrado: ' + qtd_litros_economizados);
                     resolve();
                 });
             });
         })
-	},
+    },
     /**
      * Função que atualiza o registro de volume economizado no dia
      * @param {number} volume volume obtido no dia
@@ -110,10 +110,10 @@ module.exports = {
                     callback(true);
                     reject();
                 }
-    
+
                 let sql = 'UPDATE economia_realizada SET qtd_litros_economizados = ? WHERE data = ?';
-    
-                connection.query(sql, [ qtd_litros_economizados, data ], function(error) {
+
+                connection.query(sql, [qtd_litros_economizados, data], function(error) {
                     if (error) {
                         console.error(error);
                         callback(true);
@@ -125,14 +125,14 @@ module.exports = {
                 });
             });
         })
-	},
+    },
     /**
      * Função que verifica se existe um registro de volume economizado no dia
      * @param {number} volume volume obtido no dia
      * @param {Date} data dia em que a economia foi realizada
      * @returns {Object} data e quantiade de litros
      */
-     queryEconomiaRealizada: function(data) {
+    queryEconomiaRealizada: function(data) {
         return new Promise((resolve, reject) => {
             pool.getConnection(function(error, connection) {
                 if (error) {
@@ -140,9 +140,9 @@ module.exports = {
                     callback(true);
                     reject();
                 }
-    
+
                 let sql = 'SELECT * from economia_realizada WHERE data = ?';
-    
+
                 connection.query(sql, [data], function(error, results) {
                     if (error) {
                         console.error(error);
@@ -153,12 +153,12 @@ module.exports = {
                 });
             });
         })
-	},
+    },
     /**
      * Busca o ultimo registro da tabela economia_realizada
      * @returns {number} quantiade de litros
      */
-     queryUltimaEconomiaRealizada: function() {
+    queryUltimaEconomiaRealizada: function() {
         return new Promise((resolve, reject) => {
             pool.getConnection(function(error, connection) {
                 if (error) {
@@ -166,18 +166,18 @@ module.exports = {
                     callback(true);
                     reject();
                 }
-    
+
                 let sql = 'SELECT qtd_litros_economizados FROM economia_realizada ORDER BY ID DESC LIMIT 1';
-    
+
                 connection.query(sql, [], function(error, results) {
                     if (error) {
                         console.error(error);
                         callback(true);
                         reject();
                     }
-                    resolve(results[0]);
+                    resolve(results[0].qtd_litros_economizados);
                 });
             });
         })
-	},
+    },
 };
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,5 @@
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname,"../.env") })
+require("dotenv").config({ path: path.join(__dirname, "../.env") })
 const mysql = require('../db/mysql');
 const formatacoes = require('../utility/formatacoes');
 const sensores = require('./sensores');
@@ -11,45 +11,48 @@ module.exports = {
      */
     verificaEconomiaRealizada: async function() {
         console.log('** Inicio do script **');
+        let volume_economizado = parseInt(process.env.VOLUME_ECONOMIZADO);
         let data_atual = formatacoes.convertUTCDateToLocalDate(new Date());
-        let volume_atual = process.env.VOLUME_CISTERNA1 + process.env.VOLUME_CISTERNA2
+        let volume_atual = parseInt(process.env.VOLUME_CISTERNA1) + parseInt(process.env.VOLUME_CISTERNA2)
         let registro_db = await buscaVolumeRegistrado(data_atual);
 
         // await mysql.insereEconomiaRealizada(data_atual, volume_atual)
 
-        if(data_atual !== process.env.DATA_ECONOMIA) {
+        if (data_atual !== process.env.DATA_ECONOMIA) {
 
             process.env.DATA_ECONOMIA = data_atual;
 
-            if(volume_atual > process.env.VOLUME_ECONOMIZADO) {
+            if (volume_atual > volume_economizado) {
 
-                await mysql.insereEconomiaRealizada(data_atual, volume_atual - process.env.VOLUME_ECONOMIZADO)
-
-                process.env.VOLUME_ECONOMIZADO = volume_atual;
-                console.log('valor da variavei volume_economizado atualizado para: ', process.env.VOLUME_ECONOMIZADO);
-
-            } else if(volume_atual < process.env.VOLUME_ECONOMIZADO) {
+                await mysql.insereEconomiaRealizada(data_atual, volume_atual - volume_economizado)
 
                 process.env.VOLUME_ECONOMIZADO = volume_atual;
-                console.log('valor da variavei volume_economizado atualizado para: ', process.env.VOLUME_ECONOMIZADO);
-    
+                console.log('valor da variavei volume_economizado atualizado para: ', volume_economizado);
+
+            } else if (volume_atual < volume_economizado) {
+
+                await mysql.insereEconomiaRealizada(data_atual, 0)
+
+                process.env.VOLUME_ECONOMIZADO = volume_atual;
+                console.log('valor da variavei volume_economizado atualizado para: ', volume_economizado);
+
             }
 
-        } else if(data_atual === process.env.DATA_ECONOMIA && volume_atual > process.env.VOLUME_ECONOMIZADO) {
+        } else if (data_atual === process.env.DATA_ECONOMIA && volume_atual > volume_economizado) {
 
-            let qtd_litros_economizados = registro_db.qtd_litros_economizados + (volume_atual - process.env.VOLUME_ECONOMIZADO);
-            
+            let qtd_litros_economizados = registro_db.qtd_litros_economizados + (volume_atual - parseInt(volume_economizado));
+
             await mysql.atualizaEconomiaRealizada(data_atual, qtd_litros_economizados);
 
             process.env.VOLUME_ECONOMIZADO = volume_atual;
-            console.log('valor da variavei volume_economizado atualizado para: ', process.env.VOLUME_ECONOMIZADO);
+            console.log('valor da variavei volume_economizado atualizado para: ', volume_economizado);
 
-        } else if(data_atual === process.env.DATA_ECONOMIA && volume_atual < process.env.VOLUME_ECONOMIZADO) {
+        } else if (data_atual === process.env.DATA_ECONOMIA && volume_atual < volume_economizado) {
 
             process.env.VOLUME_ECONOMIZADO = volume_atual;
-            console.log('valor da variavei volume_economizado atualizado para: ', process.env.VOLUME_ECONOMIZADO);
+            console.log('valor da variavei volume_economizado atualizado para: ', volume_economizado);
 
-        } else if(!registro_db) {
+        } else if (!registro_db) {
 
             await mysql.insereEconomiaRealizada(data_atual, 0)
 
@@ -65,9 +68,9 @@ module.exports = {
 async function buscaVolumeRegistrado(data) {
     let registro_atual;
 
-    await mysql.queryEconomiaRealizada(data).then( results => (
+    await mysql.queryEconomiaRealizada(data).then(results => (
         registro_atual = results[0]
     ));
-    
+
     return registro_atual;
 }
